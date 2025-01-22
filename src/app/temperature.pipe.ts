f38ab143b3faba10810408340d7848298de52c72
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-22 13:21:00 -0600

    Usando con más profundidad una pipe construida manualmente

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -7,15 +7,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 //PipeTransform obliga a que exista un transform ya que siempre debe existir para que una pipe funcione
 export class TemperaturePipe implements PipeTransform {
   //Esto lo usara angular cada vez que se llame esta pipe
-  transform(value: string | number) {
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
     let val: number;
+    let outputTemp: number;
+    let symbol: 'Cº' | 'ºF';
+
     if (typeof value === 'string') {
       val = parseFloat(value);
     } else {
       val = value;
     }
 
-    const outputTemp = val * (9 / 5) + 32;
-    return `${outputTemp} ºF`;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    if (!outputType) {
+      symbol = inputType == 'cel' ? 'Cº' : 'ºF';
+    } else {
+      symbol = outputType == 'cel' ? 'Cº' : 'ºF';
+    }
+    return `${outputTemp} ${symbol}`;
   }
 }
